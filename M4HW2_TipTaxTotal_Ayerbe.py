f60e7fb74b7f4f64fec639d2943f08374f764b34
XiledPhoenix <33227368+XiledPhoenix@users.noreply.github.com>
2017-12-12 16:03:51 -0500

    Add files via upload

---
+++ M4HW2_TipTaxTotal_Ayerbe.py
@@ -0,0 +1,14 @@
+#CTI-110
+#M4HW2-Tip Total
+#Guillermo Ayerbe
+#11-19-2017
+
+mealCost = int(input('What is the cost of your meal: '))
+
+foodCost = mealCost * .07
+tipCost = mealCost * .15
+
+totalCost = mealCost + foodCost + tipCost
+
+print('Your total meal cost is:' ,totalCost)
+
---
+++ M4T1_SalesPrediction_Ayerbe.py
@@ -0,0 +1,15 @@
+#CTI-110
+#M4T1 - Sales Prediction
+#Guillermo Ayerbe
+#11-13-2017
+
+#totalSales, annualProfit
+
+#Input
+totalSales = float(input('enter projected sales '))
+
+#Process
+profit = float(totalSales * .23)
+
+#Output
+print('Annual Profit is', profit)
